ed9a34af9fa40a52b7fb081bc205a4c96141820a
vbui <01bui@cardinalmail.cua.edu>
2015-12-30 00:35:47 -0500

    Add solution to Excem To CSV problem set Lesson 1

--- Problem Set 1/excel_csv.py
+++ Problem Set 1/excel_csv.py
@@ -25,14 +25,60 @@ def parse_file(datafile):
     # YOUR CODE HERE
     # Remember that you can use xlrd.xldate_as_tuple(sometime, 0) to convert
     # Excel date to Python tuple of (year, month, day, hour, minute, second)
+    # Read Excel file
+    sheet_data = [[sheet.cell_value(r, col) for col in range(sheet.ncols)] for r in range(sheet.nrows)]
+    time  = sheet.col_values(0, start_rowx=1, end_rowx=None)
+    # Find the time and value of max load for each of the regions
+    # COAST, EAST, FAR_WEST, NORTH, NORTH_C, SOUTHERN, SOUTH_C, WEST
+    temp_data = []
+    value = []
+    data = {}
+    key = ['Year', 'Month', 'Day', 'Hour', 'Max Load']
+    for i in range(1,9):
+        value = []
+        field = [item[i] for item in sheet_data][1:]
+        maxVal = max(field)
+        #maxIdx = field.index(maxVal) + 1
+        maxIdx = 0
+        for item in field:
+            maxIdx += 1
+            if item == maxVal:
+                break
+        maxTime = sheet.cell_value(maxIdx, 0)
+        maxTimeTuple = xlrd.xldate_as_tuple(maxTime, 0)
+        value = list(maxTimeTuple[:4]) # eliminate minute and second
+        print maxVal, value, maxIdx
+        value.append(maxVal)
+        temp_data.append(zip(key, value))
+    correct_stations = ['COAST', 'EAST', 'FAR_WEST', 'NORTH', 'NORTH_C', 'SOUTHERN', 'SOUTH_C', 'WEST']
+
+    i = 0
+    for item in temp_data:
+        #print item
+        data.update({correct_stations[i]: dict(item)})
+        i += 1
+    #print temp_data
+    #print data
     return data
+
 
 def save_file(data, filename):
     # YOUR CODE HERE
+    # Station|Year|Month|Day|Hour|Max Load
+    with open(filename, 'w') as csvfile:
+        fieldnames = ['Station', 'Year', 'Month', 'Day', 'Hour', 'Max Load']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter="|")
+
+        writer.writeheader()
+        keys = data.keys()
+        values = data.values()
+        for i in range(len(data)):
+            writer.writerow({'Station': keys[i], 'Year': values[i]['Year'], 'Month': values[i]['Month'], 'Day': values[i]['Day'], 'Hour': values[i]['Hour'], 'Max Load':values[i]['Max Load']})
+
 
 
 def test():
-    open_zip(datafile)
+    #open_zip(datafile)
     data = parse_file(datafile)
     save_file(data, outfile)
 
@@ -58,8 +104,8 @@ def test():
                     if field == 'Max Load':
                         max_answer = round(float(ans[station][field]), 1)
                         max_line = round(float(line[field]), 1)
+                        #print max_answer, max_line
                         assert max_answer == max_line
-
                     # Otherwise check for equality
                     else:
                         assert ans[station][field] == line[field]
