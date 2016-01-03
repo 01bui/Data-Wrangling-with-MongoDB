c2fba9259731b07890cef8fb5067dcb3f3a9e036
vbui <01bui@cardinalmail.cua.edu>
2016-01-03 17:40:59 -0500

    Add solution to airports.py

--- Problem Set 2/airports.py
+++ Problem Set 2/airports.py
@@ -12,6 +12,12 @@ def extract_airports(page):
     with open(page, "r") as html:
         # do something here to find the necessary values
         soup = BeautifulSoup(html, "lxml")
+        airportList = []
+        for item in soup.find_all('select',id="AirportList"):
+            for i in item.find_all('option'):
+                if len(i.get('value')) == 3:
+                    airportList.append(i.get('value'))
+        data = airportList[1:]
 
     return data
 
